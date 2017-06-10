eeee4440a4fdf4f3aaef9c7070e6656081fc7147
zerops <hubrical123@gmail.com>
2017-06-10 18:20:55 +0100

    some info in the docstrings

--- google-spreadsheet.py
+++ google-spreadsheet.py
@@ -1,8 +1,21 @@
+"""
+Notes:
+
+Google spreadsheet API reference: https://gspread.readthedocs.io/en/latest/
+
+IMPORTANT! Improved wrapper for gspread API: https://github.com/nithinmurali/pygsheets
+
+"""
+
+
 import gspread
+import datetime
 from oauth2client.service_account import ServiceAccountCredentials
 
 
 # use creds to create a client to interact with the Google Drive API
+start_time_utc = datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S %Z %Y")
+
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
